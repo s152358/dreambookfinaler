6b30ad03c4233d05c6b5c625f2370c8330c24482
Stefan <stefan2kwant@hotmail.com>
2017-01-09 20:22:00 +0100

    send help

--- blog/urls.py
+++ blog/urls.py
@@ -15,7 +15,7 @@ urlpatterns = [
     url(r'^comment/(?P<pk>\d+)/remove/$', views.comment_remove, name='comment_remove'),
     url(r'^accounts/register/$', views.register_page, name='register_page'),
     url(r'^api/$', views.PostsList.as_view()),
-    url(r'^api/(?P<pk>\d+)/$', views.PostsDetail.as_view()),
+    url(r'^api/(?P<pk>[0-9]+)/$', views.PostsDetail.as_view()),
 
 ]
 urlpatterns = format_suffix_patterns(urlpatterns)
